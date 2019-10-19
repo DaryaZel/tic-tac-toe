eadb0c734fa0785c671f14aad85113b9eed43fb4
Darya Zelenkova <daryazeliankova@yandex.ru>
2019-10-19 23:12:14 +0300

    feat:implement TicTacToe class

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,33 +1,79 @@
 class TicTacToe {
     constructor() {
-
+        this.currentPlayerSymbol = 'x';
+        this.playField = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ]
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayerSymbol
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.playField[rowIndex][columnIndex] ==null) {
 
+            this.playField[rowIndex][columnIndex] = this.currentPlayerSymbol;
+            if (this.currentPlayerSymbol == 'x') {
+                this.currentPlayerSymbol = 'o';
+            } else {
+                this.currentPlayerSymbol = 'x';
+            }
+        }
     }
 
     isFinished() {
-
+        if (this.getWinner() || this.isDraw()) {
+            return true
+        }
+        return false
     }
 
     getWinner() {
-
+        if (this.playField[1][1] != null
+            && (this.playField[0][0] == this.playField[1][1]
+                && this.playField[1][1] == this.playField[2][2]) ||
+            (this.playField[0][2] === this.playField[1][1]
+                && this.playField[1][1] == this.playField[2][0])) {
+            return this.playField[1][1];
+        }
+        for (let i = 0; i < 3; i++) {
+            if (this.playField[0][i] != null
+                && this.playField[0][i] == this.playField[1][i]
+                && this.playField[1][i] == this.playField[2][i]) {
+                return this.playField[0][i];
+            }
+            else if (this.playField[i][0] != null
+                && this.playField[i][0] == this.playField[i][1]
+                && this.playField[i][1] == this.playField[i][2]) {
+                return this.playField[i][0];
+            }
+        }        
+        return null;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (!this.playField[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     isDraw() {
-
+        if (this.getWinner() == null && this.noMoreTurns()) {
+            return true
+        }
+        return false
     }
 
     getFieldValue(rowIndex, colIndex) {
+        return this.playField[rowIndex][colIndex]
 
     }
 }
